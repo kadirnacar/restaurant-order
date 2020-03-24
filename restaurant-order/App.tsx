85f50b28fb154f94926f0236b52aeb22f16d4f0b
Kadir Niyazi AÇAR <kadirnacar@kadirnacar.com>
2020-03-24 10:12:39 +0300

    splash and icon

--- restaurant-order/App.tsx
+++ restaurant-order/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ImageBackground source={require("./assets/background.jpg")} style={{ width: "100%", height: "100%" }} >
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(33,36,45,0.9)',
     alignItems: 'center',
     justifyContent: 'center',
   },
